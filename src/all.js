3b94d28c704b981d0ef8deda80c45c6ee6da7cc6
Sin <102574135+yusin07@users.noreply.github.com>
2022-09-20 18:15:01 +0800

    mobile bug fix

--- src/all.js
+++ src/all.js
@@ -135,7 +135,7 @@ AOS.init({
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 200, // offset (in px) from the original trigger point
+    offset: 160, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 750, // values from 0 to 3000, with step 50ms
     easing: "ease", // default easing for AOS animations
