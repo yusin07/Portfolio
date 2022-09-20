ff6c1b8f2fcbfcfdd1c2e9e85cc3a34d3d78416e
Sin <102574135+yusin07@users.noreply.github.com>
2022-09-20 18:36:40 +0800

    mobile bug fix

--- src/all.js
+++ src/all.js
@@ -137,7 +137,7 @@ AOS.init({
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 160, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 750, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
     easing: "ease", // default easing for AOS animations
     once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
